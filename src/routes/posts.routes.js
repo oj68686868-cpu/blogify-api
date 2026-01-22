e8113e530b82d0f722526a4af64518cef33eedae
Om Jagtap <omjagtapatil07@gmail.com>
2026-01-22 15:35:47 +0530

    refactor: move post logic to controller

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,15 +1,12 @@
 // src/routes/posts.routes.js
 const express = require('express');
-
-// 1. Create a router instance (The "Mini-App")
 const router = express.Router();
 
-// 2. Define routes on the router
-// Note: We use '/' because this router will be mounted at '/api/v1/posts' later.
-// So, '/' here actually means '/api/v1/posts/' in the real world.
-router.get('/', (req, res) => {
-    res.send('Fetching all blog posts from the modular router!');
-});
+// 1. Import the controller
+const postsController = require('../controllers/posts.controller');
 
-// 3. Export the router so index.js can use it
+// 2. Use the controller function
+// Notice we don't use () after getAllPosts. We are passing the function itself.
+router.get('/', postsController.getAllPosts);
+
 module.exports = router;
